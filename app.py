5a59fdb73309c903c6a36ebeca789d7ab84c2839
Niki Ramandika <nikiramandika@gmail.com>
2025-06-17 17:10:02 +0700

    update lagi

--- app.py
+++ app.py
@@ -452,6 +452,8 @@ def handle_send_message(data):
     conn.close()
 
 
+# Pastikan init_db() dipanggil di luar blok if __name__ == '__main__'
+init_db()
+
 if __name__ == '__main__':
-    init_db()
     socketio.run(app, debug=True, host='0.0.0.0', port=5000)
